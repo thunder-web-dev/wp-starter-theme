f0f7d6d384ff47396998d5e8d330b5948d2a15d0
Timofey <drake9100@mail.ru>
2025-03-28 15:57:49 +0300

    UPD: Замена import на forward

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -6,7 +6,7 @@ import gulpSass from 'gulp-sass';
 import plumber from 'gulp-plumber';
 import autoprefixer from 'gulp-autoprefixer';
 import notify from 'gulp-notify';
-import gulpSassGlob from 'gulp-sass-glob'
+import gulpSassGlob from 'gulp-sass-glob-use-forward'
 
 const sass = gulpSass(dartSass);
 
